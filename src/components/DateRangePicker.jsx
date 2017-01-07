2bd7fcd48f0f470e1f0614269662f92f766f526f
valentyn.grekulov <grekulov.valentyn@pdffiller.team>
2017-01-07 15:46:12 +0200

    IE fix

--- src/components/DateRangePicker.jsx
+++ src/components/DateRangePicker.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import moment from 'moment';
 import cx from 'classnames';
 import Portal from 'react-portal';
+import classnames from 'classnames';
 
 import isTouchDevice from '../utils/isTouchDevice';
 import getResponsiveContainerStyles from '../utils/getResponsiveContainerStyles';
@@ -267,7 +268,10 @@ export default class DateRangePicker extends React.Component {
     } = this.props;
 
     this.focusedInputArr.push(focusedInput);
-    console.log(this.focusedInputArr);
+    let buttonClass = classnames("g-btn g-btn-auto-width g-btn-primary-empty g-btn--sm myac-audit-trails-datapicker__button", {
+      "hide": !(this.focusedInputArr.length > 1 && (this.focusedInputArr[this.focusedInputArr.length - 2] === START_DATE && this.focusedInputArr[this.focusedInputArr.length - 1] === END_DATE))
+    });
+
     return (
       <div className="DateRangePicker">
         <DateRangePickerInputController
@@ -293,7 +297,7 @@ export default class DateRangePicker extends React.Component {
           phrases={phrases}
         />
 
-        <button className="g-btn g-btn-auto-width g-btn-primary-empty g-btn--sm myac-audit-trails-datapicker__button" onClick={() => {
+        <button className={buttonClass} onClick={() => {
           onDatesApply(startDate, endDate);
           onFocusChange(null);
         }}><i className="i i-check"></i><span>{buttonName}</span>
